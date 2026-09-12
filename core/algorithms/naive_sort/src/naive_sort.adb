684710a10efd98a8487991a5ea33cfb435bc9727
yorche3 <hyaoki123@gmail.com>
2026-09-11 23:10:31 -0600

    feat(algorithms): add insertion sort implementation

--- core/algorithms/naive_sort/src/naive_sort.adb
+++ core/algorithms/naive_sort/src/naive_sort.adb
@@ -75,4 +75,36 @@ package body Naive_Sort is
       end loop;
       return Result;
    end Bubble_Sort;
+
+   -- Insertion_Sort : sorts an array of integers using the insertion sort algorithm
+   -- Input: Arr - an array of integers to be sorted
+   -- Output: a new array containing the sorted integers from Arr
+   function Insertion_Sort (Arr : Integer_Array) return Integer_Array is
+      Result : Integer_Array (Arr'Range) := Arr;
+      N : constant Natural := Arr'Length;
+   begin
+      -- verify if arr is null or empty and returns empty array
+      if Arr'Length = 0 then
+         return Result;
+      end if;
+      -- if n <= 1, the array is already sorted
+      if N <= 1 then
+         return Result;
+      end if;
+
+      -- perform insertion sort
+      for I in Result'First + 1 .. N - 1 loop
+         declare
+            Key : Integer := Result(I);
+            J : Integer := I - 1;
+         begin
+            while J >= 0 and then Result(J) > Key loop
+               Result(J + 1) := Result(J);
+               J := J - 1;
+            end loop;
+            Result(J + 1) := Key;
+         end;
+      end loop;
+      return Result;
+   end Insertion_Sort;
 end Naive_Sort;
